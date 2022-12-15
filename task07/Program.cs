6e4f5659eb8e9e5269abb4d93714deec5c63f814
APavlov <markmarkovkin@gmail.com>
2022-12-15 12:35:50 +0300

    Create project 5 and 7. task05 and task07

---
+++ task07/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное число:");
+int usernumber = Convert.ToInt32 (Console.ReadLine());
+
+if ( usernumber > 100 && usernumber < 1000 ){
+    Console.WriteLine( $"Последняя цифра числа {usernumber} это: {usernumber%10}" );
+} else {
+    Console.WriteLine( "Вы ввели не трех значное число!" );
+}
