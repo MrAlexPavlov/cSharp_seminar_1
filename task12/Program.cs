45b83fd948dacbcde615937fb31422cc3160475c
APavlov <markmarkovkin@gmail.com>
2022-12-19 11:56:03 +0300

    small fix

--- task12/Program.cs
+++ task12/Program.cs
@@ -6,20 +6,36 @@
 // 16, 4 -> кратно
 
 Console.WriteLine("Введите число:");
-int usernumber = Convert.ToInt32 (Console.ReadLine());
+int userNumber = Convert.ToInt32 (Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int usernumber2 = Convert.ToInt32 (Console.ReadLine());
-
-string Proverka (int us1, int us2)
+int userNumber2 = Convert.ToInt32 (Console.ReadLine());
+/*
+string DivCheck (int number1, int number2)
 {
     if ( us1 % us2 == 0 ) 
     {
-        return $"{us1}, {us2} -> кратно" ;
+        return $"{number1}, {number2} -> кратно" ;
     } else {
-        return $"{us1}, {us2} -> не кратно, остаток "+(us1 % us2);
+        return $"{number1}, {number2} -> не кратно, остаток "+(number1 % number2);
     }
 }
-string res = Proverka(usernumber,usernumber2);
-Console.WriteLine(res);
+*/
+bool DivCheck (int number1, int number2)
+{
+    if ( number1 % number2 == 0 ) 
+    {
+        return true ;
+    } else {
+        return false;
+    }
+}
+
+if ( DivCheck(userNumber,userNumber2) ) 
+{
+    Console.WriteLine($"{userNumber}, {userNumber2} -> кратно");
+} else 
+{
+Console.WriteLine($"{userNumber}, {userNumber2} -> не кратно, остаток "+(userNumber % userNumber2));
+}
 
