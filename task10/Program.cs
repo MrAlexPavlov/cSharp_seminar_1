b69e865268fd4290f68c28638761fed0ecf48132
APavlov <markmarkovkin@gmail.com>
2022-12-20 10:04:21 +0300

    final editing for task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -6,24 +6,24 @@
 // 782 -> 8
 // 918 -> 1
 
-int SecondDigit(int digit)//Метод для получения 2 цифры
+int SecondDigit(int digit)//Метод для получения 2 цифры трехзначного числа
 {
     digit = digit / 10;
     return digit % 10;
 }
 
-int InsertDigit() //Метод ввода пользовательских данных
+int InsertDigit(string text) //Метод ввода пользовательских числовых значений
 {
-    Console.WriteLine("Введите трех значаное число");
+    Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
 }
 
 
-//Просим пользователья ввести трех значное число, будем просить пока не введет правильно.
+//Просим пользователья ввести трех значное число. Будем просить пока не введет правильно.
 int usNumber = 0;
 while (usNumber<100 || usNumber>999) 
 {
-    usNumber = InsertDigit(); // вызов метода для ввода числа от пользователя
+    usNumber = InsertDigit("Введите трех значаное число"); // вызов метода для получения числа от пользователя
     if (usNumber<100 || usNumber>999)
     {
         Console.WriteLine($"{usNumber} - не трехзначное число\n");
