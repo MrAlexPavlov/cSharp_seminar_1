b65b629f2bfc50d11a1c08d07e3ae9f5b1e8b595
APavlov <markmarkovkin@gmail.com>
2023-01-16 11:06:38 +0300

    resolved task 33

---
+++ task33/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreatArray(int arraySize, int arrayMinValue, int arrayMaxValue)//Метод генерации массива со случайными элементами
+{
+    int[] resArray = new int[arraySize];
+    Random rnd = new Random();
+    for (int i = 0; i < resArray.Length; i++)
+    {
+        resArray[i] = rnd.Next(arrayMinValue, arrayMaxValue + 1);
+    }
+    return resArray;
+}
+
+
+string ArrayToString(int[] array)//Метод преобразования массива в строку
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + $" {array[i]}";
+        result = i < (array.Length - 1) ? result = result + "," : result;//для корректности вывода запятых
+    }
+    return result + " ]";
+}
+
+
+bool Finddigit(int[] array, int f)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]==f) return true;
+    }
+    return false;
+}
+
+int arraySize = 5, arrayMinValue = 0, arrayMaxValue = 10, find = 3;
+
+int[] newArray = CreatArray(arraySize, arrayMinValue, arrayMaxValue); //генерируем массив с указаными параметры
+string strArray = ArrayToString(newArray);// переводим строку в массив для дальнейшего вывода на экран
+
+if (Finddigit(newArray,find)) 
+{
+    Console.WriteLine($"{find}; массив {strArray} -> да ");
+} 
+else 
+{
+    Console.WriteLine($"{find}; массив {strArray} -> нет ");
+}
