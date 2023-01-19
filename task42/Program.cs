ce11233a7911193fa3a809b6afc62f4606d6715e
APavlov <markmarkovkin@gmail.com>
2023-01-19 14:06:53 +0300

    add 3 methods for get binary from decimal

--- task42/Program.cs
+++ task42/Program.cs
@@ -6,41 +6,50 @@
 
 int InsertDigit(string text) //Метод пользовательского ввода для целых чисел
 {
-    int result;
-    bool parse;
+    int result;bool parse;
     Console.WriteLine(text);
     parse = Int32.TryParse(Console.ReadLine(), out result);
     if (!parse || result <= 0) result = InsertDigit(text);//Если пользователь ввел некорректное значение вызываем повтороно метод.
     return result;
 }
 
-int ReverseInt(int a) //метод разворота натурального числа
+double DecimalToBinary(int a)//Метод преобразования десятичного натурального числа в двоичное циклом While
 {
-    int res = 0;
-    if (a == 1) return 10;
+    double res = 0;
+    int countPower = 0;
     while (a > 0)
     {
-        res = res * 10 + a % 10;
-        a = a / 10;
+        res = res + a % 2 * Math.Pow(10, countPower);//Каждая последующая цифра должна попадать в следующий регистр от предидущего
+        a /= 2;
+        countPower++;
     }
     return res;
 }
 
-int DecimaltoBool(int a)//Метод преобразования десятичного натурального числа в двоичное
+int DecimalToBinaryRecursive(int a)//Рекурсивный метод преобразования десятичного натурального числа в двоичное
 {
     int res = 0;
-    while (a > 0)
-    {
-        res = res * 10 + a % 2;
-        //res = res * 10 + a % 2;
-        a /= 2;
-    }
-    //return res;
-    return ReverseInt(res);
+    if (a == 0) return res;
+    res = DecimalToBinaryRecursive(a / 2);
+    res = res * 10 + a % 2;
+    return res;
 }
 
-int usNumber = InsertDigit("Введите натуральное число:");
+int DecimalToBinaryString(int a)//Метод преобразования десятичного натурального числа в двоичное, используя строку
+{
+    string res="";
+    while (a > 0)
+    {
+        res = a%2 + res;
+        a /=2;
+    }
+    return Convert.ToInt32(res);
+}
 
-int res = DecimaltoBool(usNumber);
+int usNumber = InsertDigit("Введите натуральное число:");//Просим пользователя ввести натуральное число
 
-Console.WriteLine(res);
+double res = DecimalToBinary(usNumber); //1 метод преобразованя из 10 в 2. Цикл while
+int resRecursive = DecimalToBinaryRecursive(usNumber);//2 метод преобразованя из 10 в 2. Рекурсия
+int resStringToInt = DecimalToBinaryString(usNumber);//3 метод преобразования из 10 в 2. Строка
+
+Console.WriteLine($"{res} = {resRecursive} = {resStringToInt}");
