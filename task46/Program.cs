b9888f121cee84906a8a3101a8d430ab59bc74af
APavlov <markmarkovkin@gmail.com>
2023-01-23 15:35:39 +0300

    resolved task46

---
+++ task46/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+int[,] CreatMatrixRndIntArray(int rowSize, int colSize, int minValue, int maxValue)//Метод генерации массива со случайными элементами
+{
+    int[,] resArray = new int[rowSize, colSize];
+    Random rnd = new Random();
+
+    for (int i = 0; i < resArray.GetLength(0); i++)
+        for (int j = 0; j < resArray.GetLength(1); j++)
+            resArray[i, j] = rnd.Next(minValue, maxValue + 1);
+
+    return resArray;
+}
+
+string ArrayToString(int[,] array)//Метод преобразования массива в строку
+{
+    string result = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        result += "[";
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result = result + $" {array[i, j],3}";
+            result = j < (array.GetLength(1) - 1) ? result + "," : result;//для корректности вывода запятых    
+        }
+
+        result += "]\n";
+    }
+    return result;
+}
+
+//Параметры для генерации двумерного массива типа int.
+int matrixRowSize = 5,
+    matrixColSize = 4,
+    matrixMinValue = -99,
+    matrixMaxValue = 99;
+
+int[,] rndMatrixArray = CreatMatrixRndIntArray(matrixRowSize, matrixColSize, matrixMinValue, matrixMaxValue);
+
+string rndMatrixString = ArrayToString(rndMatrixArray);
+Console.Clear();
+Console.WriteLine(rndMatrixString);
