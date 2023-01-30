c4c167eb4b3379e67c1d761f29b075012a69795d
APavlov <markmarkovkin@gmail.com>
2023-01-30 11:30:15 +0300

    resolved task 67

---
+++ task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int InsertDigit(string text) //Метод пользовательского ввода для целых чисел
+{
+    int result; bool parse;
+    Console.WriteLine(text);
+    parse = Int32.TryParse(Console.ReadLine(), out result);
+    if (!parse) result = InsertDigit(text);//Если пользователь ввел некорректное значение вызываем повтороно метод.
+    return result;
+}
+
+int SumDigit(int n)
+{
+    if (n == 0) return 0;
+    return SumDigit(n / 10) + n % 10;
+}
+
+int nUser = InsertDigit("Введите число N:");
+
+int sum = SumDigit(nUser);
+Console.WriteLine(sum);
